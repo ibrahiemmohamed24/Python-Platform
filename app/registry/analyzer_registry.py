95cd38237a924d038fb02eb3b0baa68aa3f6d098
ibrahiemmohamed24 <hzizo964@gmail.com>
2026-07-13 17:23:32 +0300

    Use analyzer factory in registry

--- app/registry/analyzer_registry.py
+++ app/registry/analyzer_registry.py
@@ -1,5 +1,5 @@
 from app.analyzers.analyzer_manager import AnalyzerManager
-
+from app.factory.analyzer_factory import AnalyzerFactory
 from app.analyzers.ruff_analyzer import RuffAnalyzer
 from app.analyzers.bandit_analyzer import BanditAnalyzer
 from app.analyzers.mypy_analyzer import MypyAnalyzer
@@ -10,8 +10,8 @@ class AnalyzerRegistry:
     @staticmethod
     def build() -> AnalyzerManager:
         manager = AnalyzerManager()
-        manager.register(RuffAnalyzer())
-        manager.register(BanditAnalyzer())
-        manager.register(MypyAnalyzer())
+        manager.register(AnalyzerFactory.create(RuffAnalyzer))
+        manager.register(AnalyzerFactory.create(BanditAnalyzer))
+        manager.register(AnalyzerFactory.create(MypyAnalyzer))
 
         return manager
